92d6f07bc4b7c37ba99273c57428ee5f19a87bf1
VPPI\richard.puig <richard.puig@veritaspay.com>
2024-04-30 21:57:07 +0800

    Changed location

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
 
       <div className='mt-5 flex flex-col gap-3 text-slate-500'>
         <p>
-          Software Engineer based in Croatia, specializing in technical
+          Software Engineer based in the Philippines, specializing in technical
           education through hands-on learning and building applications.
         </p>
       </div>
