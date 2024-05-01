fbb927510f3b1e436f44fb6f8100561a2265a5de
VPPI\richard.puig <richard.puig@veritaspay.com>
2024-05-02 02:38:38 +0800

    Changed loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,15 @@
 import { Html } from "@react-three/drei";
+import ReactLoader from "react-js-loader";
 
 const Loader = () => {
+  const color = '#ded8d7';
   return (
     <Html>
-      <div className='flex justify-center items-center'>
-        <div className='w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin'></div>
-      </div>
+        <div className={'absolute left-0 right-0 z-10 flex items-center justify-center'}>
+            <div className={"item"}>
+                <ReactLoader type="bubble-loop" bgColor={color} color={color} size={100} />
+            </div>
+        </div>
     </Html>
   );
 };
