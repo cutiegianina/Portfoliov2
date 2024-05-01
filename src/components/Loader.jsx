dda7ec6c81fa7e08411e15e2b420d5257bdfcf42
VPPI\richard.puig <richard.puig@veritaspay.com>
2024-05-02 03:32:55 +0800

    Adjusted loader size

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,6 @@
 import { Html } from "@react-three/drei";
 import ReactLoader from "react-js-loader";
+import cartoonLoader from '../assets/gifs/cartoon-loader.gif';
 
 const Loader = () => {
   const color = '#ded8d7';
@@ -8,14 +9,13 @@ const Loader = () => {
         <div className={'absolute left-0 right-0 z-10 flex items-center justify-center'}>
             <div className={"item"}>
                 <CartoonLoader />
-                <ReactLoader type="bubble-loop" bgColor={color} color={color} size={1} />
+                <ReactLoader type="bubble-loop" bgColor={color} color={color} size={50} />
             </div>
         </div>
     </Html>
   );
 };
 
-import cartoonLoader from '../assets/gifs/cartoon-loader.gif';
 
 const CartoonLoader = () => {
   return (
